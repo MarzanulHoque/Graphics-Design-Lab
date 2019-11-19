2ba02c85c41a735d48c9c877f7094a4b59ebb996
MarzanulHoque <37240887+MarzanulHoque@users.noreply.github.com>
2019-11-18 22:37:00 -0800

    Creating an Elipse using c++

---
+++ Elipse.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+using namespace std;
+int main() {
+   int gd = DETECT, gm;
+   initgraph(&gd, &gm, "");
+   ellipse(300, 200, 0, 360, 200, 100);
+   getch();
+   closegraph();
+   return 0;
+}
